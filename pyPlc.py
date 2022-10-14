d29ca27a78d56f0538df292684cd7072943037a0
uhi22 <uhi@hnng.de>
2022-10-14 18:11:01 +0200

    added tkinter top level module and submodules

---
+++ pyPlc.py
@@ -0,0 +1,58 @@
+
+# This is a simple Tkinter program, running a main loop and reacting on keys
+#
+# Tested on Windows10 with python 3.9
+#
+# https://groups.google.com/g/comp.lang.python/c/dldnjWRX3lE/m/cL69gG3fCAAJ
+
+#------------------------------------------------------------
+import tkinter as tk
+import time
+import pyPlcWorker
+
+def storekeyname(event):
+    global nKeystrokes
+    global lastKey
+    nKeystrokes+=1
+    lastKey = event.keysym
+    worker.handleUserAction(lastKey)
+    return 'break' # swallow the event
+
+def inkey():
+    global lastKey
+    return lastKey
+    lastKey = ''
+
+def cbAddToTrace(s):
+    print(s)
+
+def cbShowStatus(s):
+    print(s)
+    lblStatus['text']=s
+    root.update()
+
+root = tk.Tk()
+lastKey = ''
+display = tk.Label(root, text='No Key', width=30) # A textual element in the graphical user interface
+display.pack()
+lblHelp = tk.Label(root, text="x=exit, t=testframe")
+lblHelp.pack()
+lblStatus = tk.Label(root, text="(Status)")
+lblStatus.pack()
+# Bind the keyboard handler to all relevant elements:
+display.bind('<Key>', storekeyname)
+root.bind('<Key>', storekeyname)
+cbShowStatus("initialized")
+root.update()
+worker=pyPlcWorker.pyPlcWorker(cbAddToTrace, cbShowStatus)
+
+nMainloops=0
+nKeystrokes=0
+while lastKey!="x":
+    time.sleep(.3) # 'do some calculation'
+    nMainloops+=1
+    # print(str(nMainloops) + " " + str(nKeystrokes)) # show something in the console window
+    root.update()
+    worker.mainfunction()
+        
+#---------------------------------------------------------------
---
+++ pyPlcWorker.py
@@ -0,0 +1,41 @@
+# Worker for the pyPLC
+#
+# Tested on Windows10 with python 3.9
+#
+
+#------------------------------------------------------------
+import pyPlcHomeplug
+
+
+
+
+class pyPlcWorker():
+    def __init__(self, callbackAddToTrace=None, callbackShowStatus=None):
+        print("initializing pyPlcWorker") 
+        self.something = "Hallo das ist ein Test"
+        self.nMainFunctionCalls=0
+        self.strUserAction = ""
+        self.callbackAddToTrace = callbackAddToTrace
+        self.callbackShowStatus = callbackShowStatus
+        self.hp = pyPlcHomeplug.pyPlcHomeplug(self.callbackAddToTrace, self.callbackShowStatus)
+ 
+    def addToTrace(self, s):
+        self.callbackAddToTrace(s)
+        
+    def showStatus(self, s):
+        self.callbackShowStatus(s)        
+        
+    def mainfunction(self):
+        self.nMainFunctionCalls+=1
+        self.showStatus("pyPlcWorker loop " + str(self.nMainFunctionCalls))
+        #self.hp.mainfunction()
+        
+
+
+    def handleUserAction(self, strAction):
+        self.strUserAction = strAction
+        self.addToTrace("UserAction " + strAction)
+        if (strAction == "t"):
+            self.addToTrace("sending test frame")
+            self.hp.sendTestFrame()
+
