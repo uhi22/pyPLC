00d7cee357e6d4df6f2b404d80175930809b7b2d
Uwe Hennig <uhi@hnng.de>
2023-02-27 12:39:11 +0100

    feature: for autostart on RPi, removed starter.py and directly call pevNoGui.py

--- starter.sh
+++ starter.sh
@@ -3,6 +3,6 @@
 date >> /home/pi/myprogs/pyPlc/test2.txt
 pwd >> /home/pi/myprogs/pyPlc/test2.txt
 cd /home/pi/myprogs/pyPlc/
-/usr/bin/python /home/pi/myprogs/pyPlc/starter.py
+/usr/bin/python /home/pi/myprogs/pyPlc/pevNoGui.py
 pwd >> /home/pi/myprogs/pyPlc/test2.txt
 date >> /home/pi/myprogs/pyPlc/test3.txt
