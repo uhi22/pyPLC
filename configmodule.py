82f08c7c4a9b249a69510e6c0d9f0499e104ab21
uhi22 <uhi@hnng.de>
2023-05-12 08:17:17 +0200

    feature: print help if ini file is missing

--- configmodule.py
+++ configmodule.py
@@ -3,12 +3,20 @@
 # See https://docs.python.org/3/library/configparser.html
 
 import configparser
+import sys
 
 config = configparser.ConfigParser()
 config.read('pyPlc.ini')
 
 def getConfigValue(s):
-    return config['general'][s]
+    try:
+        return config['general'][s]
+    except:
+        print("Error: seems we have a problem with pyPlc.ini.")
+        print("How to fix: Try to use the docs/pyPlc.ini.template,")
+        print(" copy it into the pyPlc's main folder,")
+        print(" rename it to pyPlc.ini, and edit it for your needs.")
+        sys.exit()
 
 def getConfigValueBool(s):
     return config.getboolean('general', s)
